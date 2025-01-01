72073aa9ab728ca53d1efe38bbafc97f5be86471
Hemalatha <163089868+Hemalatha172307@users.noreply.github.com>
2025-01-01 14:53:07 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './App.css'
+import Navbar from './components/Navbar'
+
+import About from './components/About'
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <About />
+      
+    </div>
+  )
+}
+
+export default App

214e78957448b047cda25eca19b0729689dbcfe0
Hemalatha <163089868+Hemalatha172307@users.noreply.github.com>
2025-01-10 22:31:34 +0530

    index.js

--- About/index.js
+++ About/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './index.css'
-import { FaFacebookF } from "react-icons/fa"
-import { FaLinkedinIn } from "react-icons/fa6"
-import { FaSkype } from "react-icons/fa"
-import { FaTwitter } from "react-icons/fa"
+import { FaFacebookF, FaLinkedinIn, FaSkype, FaTwitter } from "react-icons/fa"
 import image from '../../assets/home.png'
-const About = () =>  {
+
+
+const Home = () => {
   return (
-    <div className='home-section'>
+    <div className='home-section' id="home">
       <div className='text-section'>
         <h4>FrontEnd Developer</h4>
         <div className='text-container'>
@@ -17,20 +16,20 @@ const About = () =>  {
           <button className='btn1'>View Projects</button>
         </div>
         <div className='image-container'>
-            <img src={image} alt="home-image"/>
+          <img src={image} alt="home-image"/>
         </div>
         <div className='share'>
           <p className='paragraph'>Check out my:</p>
-            <div className='social'>
-              <button><FaFacebookF /></button>
-              <button><FaTwitter/></button>
-              <button><FaLinkedinIn /></button>
-              <button><FaSkype /></button>
-            </div>
+          <div className='social'>
+            <button><FaFacebookF /></button>
+            <button><FaTwitter/></button>
+            <button><FaLinkedinIn /></button>
+            <button><FaSkype /></button>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default About
+export default Home

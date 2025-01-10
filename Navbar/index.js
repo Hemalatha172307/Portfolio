ddc83ccf86e5baa7a77e9d0fd38e27dd1ca9f406
Hemalatha <163089868+Hemalatha172307@users.noreply.github.com>
2025-01-10 22:25:02 +0530

    index.js

--- Navbar/index.js
+++ Navbar/index.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import { FaBars } from "react-icons/fa6";
+import { HashLink as Link } from 'react-router-hash-link';  
+
 import './index.css'
+const Navbar = () => {  
+    return (  
+        <nav className='navbar'>  
+            <div className='navbar-logo'>  
+                <h1>H<span>emalatha</span></h1>  
+            </div>  
 
-const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-logo'>
-        <h1>H<span>emalatha</span></h1>
-      </div>
-      <ul className='nav-menu'>
-        <li className='active'>Home</li>
-        <li>About</li>
-        <li>Service</li>
-        <li>Portfolio</li>
-        <li>Blog</li>
-      </ul>
-      <div className='nav-btn'>
-        <button>Connect With Me</button>
-        <FaBars className='menu-bar' />
-      </div>
-      
-    </nav>
-  )
-}
+            <ul className='nav-menu'>  
+                <li>  
+                    <Link className='nav-link' to="#home" smooth={true}>Home</Link>  
+                </li>  
+                <li>  
+                    <Link className='nav-link' to="#about" smooth={true}>About</Link>  
+                </li>  
+                <li>  
+                    <Link className='nav-link' to="#skills" smooth={true}>Skills</Link>  
+                </li>  
+                <li>  
+                    <Link className='nav-link' to="#services" smooth={true}>Services</Link>  
+                </li>  
+                <li>  
+                    <Link className='nav-link' to="#portfolio" smooth={true}>Portfolio</Link>  
+                </li>  
+            </ul>  
 
-export default Navbar
+            <div className='nav-btn'>  
+                <button>Connect With Me</button>  
+            </div>  
+        </nav>  
+    );  
+};  
+
+export default Navbar;
